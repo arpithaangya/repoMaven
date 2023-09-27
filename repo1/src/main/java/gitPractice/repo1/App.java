fd4bd6dc20e75a5bf08a1ff52e5ef5a5a26b21b5
ashekharangya <ashekharangya@TMZ-DPC-2185.ardianet.net>
2023-09-27 11:55:18 +0530

    hiii

---
+++ repo1/src/main/java/gitPractice/repo1/App.java
@@ -0,0 +1,13 @@
+package gitPractice.repo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
